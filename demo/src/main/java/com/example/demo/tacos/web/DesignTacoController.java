2510666c1d39a6e1c00ee3c87532061ff6af6027
Nguyen Vinh Hung <52200097@student.tdtu.edu.vn>
2025-02-15 10:57:55 +0700

    Update chapter 2 Part I

--- demo/src/main/java/com/example/demo/tacos/web/DesignTacoController.java
+++ demo/src/main/java/com/example/demo/tacos/web/DesignTacoController.java
@@ -7,10 +7,7 @@ import com.example.demo.tacos.TacoOrder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -53,6 +50,15 @@ public class DesignTacoController {
     public String showDesignForm() {
         return "design";
     }
+
+    @PostMapping
+    public String processTaco(Taco taco,
+                              @ModelAttribute TacoOrder tacoOrder) {
+        tacoOrder.addTaco(taco);
+        log.info("Processing taco: {}", taco);
+        return "redirect:/orders/current";
+    }
+
     private Iterable<Ingredient> filterByType(
             List<Ingredient> ingredients, Type type) {
         return ingredients
